22a1be260bc012f31f9685685233bce616070315
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-24 15:25:46 +0600

    section title dynamic done

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SectionTitle = ({ heading, subHeading }) => {
+    return (
+        <>
+            <div className="mx-auto text-center md:w-4/12 my-8">
+                <p className="text-[#393950] mb-2"> --- {subHeading}</p>
+                <h3  className="text-3xl text-[#332FD0]  uppercase border-y-4 rounded-md py-4">{heading}</h3>
+            </div>
+        </>
+    );
+};
+
+export default SectionTitle;
