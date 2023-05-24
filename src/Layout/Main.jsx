e297ba84f7f07c03994db47d94fb71c025b41a2d
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-24 12:32:41 +0600

    navbar done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
+import Home from '../Pages/Home/Home/Home';
+import NavBar from '../Pages/Shared/NavBar/NavBar';
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
---
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Logo from '../../../assets/Logo.png'
+import { Link } from 'react-router-dom';
+const NavBar = () => {
+    const navOptions = <>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
+        <li><Link to="/order/salad">Order Food</Link></li>
+
+    </>
+
+    return (
+        <>
+            <div className="conatainer mx-auto navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {navOptions}
+                        </ul>
+                    </div>
+                    <img src={Logo} alt="" className="w-full h-auto sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5" />
+
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navOptions}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn bg-[#332FD0] border-none text-white">Book Now</a>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default NavBar;
