1cd7fde94951a6bf4a8ff3ccab6ae0521e05b1cf
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-24 15:16:06 +0600

    chef service section done

---
+++ src/Pages/Home/ChefService/ChefService.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import BackgroundImg from '../../../assets/home/chef-service.jpg'
+const Hero = () => {
+    return (
+        <>
+            <div className="my-3 hero min-h-screen" style={{ backgroundImage: `url(${BackgroundImg})` }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="bg-white px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 py-8 sm:py-12 md:py-16 lg:py-20 xl:py-24">
+                        <div className="max-w-md mx-auto">
+                            <h1 className="mb-4 text-3xl sm:text-4xl md:text-5xl font-bold text-[#393950]">SM RESTRO SHOP</h1>
+                            <p className="mb-4 text-base sm:text-lg md:text-xl text-[#393950]">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default Hero;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Catagory from '../Catagory/Catagory';
+import Hero from '../ChefService/ChefService';
+import ChefService from '../ChefService/ChefService';
 
 const Home = () => {
     return (
         <>
            <Banner></Banner>
            <Catagory></Catagory>
+           <ChefService></ChefService>
         </>
     );
 };
