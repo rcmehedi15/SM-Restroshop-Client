5f4cec2b61881e21adef94a8e86dd01a9a22c730
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-24 16:41:16 +0600

    call us section don

---
+++ src/Pages/Home/CallUs/CallUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CallUs = () => {
+    return (
+        <>
+            <div className="bg-purple-800 text-white text-center py-4 sm:py-6 md:py-8 lg:py-10">
+                <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl">Call us +880198961843</p>
+            </div>
+
+        </>
+    );
+};
+
+export default CallUs;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ import Catagory from '../Category/Category';
 import Hero from '../ChefService/ChefService';
 import ChefService from '../ChefService/ChefService';
 import PopularMenu from '../PopularMenu/PopularMenu';
+import CallUs from '../CallUs/CallUs';
 
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
            <Catagory></Catagory>
            <ChefService></ChefService>
            <PopularMenu></PopularMenu>
+           <CallUs></CallUs>
         </>
     );
 };
