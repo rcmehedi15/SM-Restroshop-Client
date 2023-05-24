6467b0594fa3e073325f0624144dd85e2ba3969d
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-24 14:33:28 +0600

    carousel done

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import { router } from './Routes/Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-       <RouterProvider router={router} />
+    <div>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
