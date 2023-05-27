8cba496e6afd91c1de52c082fac27b7e2eab26ce
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-27 14:04:30 +0600

    salad tab done

---
+++ src/Components/SectionTitle/FoodCard/FoodCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const FoodCard = ({ item }) => {
+    const { name, image, price, recipe, } = item;
+
+    return (
+        <>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <img src={image} alt="Shoes" className='h-64'/>
+                <p className="absolute  mr-4 mt-4 px-4 bg-[#393950] text-white">${price}</p>
+                <div className="card-body flex flex-col items-center">
+                    <h2 className="card-title">{name}</h2>
+                    <p>{recipe}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-outline hover:bg-[#332FD0] mt-4">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default FoodCard;
--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -3,9 +3,19 @@ import Cover from '../../Shared/Cover/Cover';
 import orderCoverImg from '../../../assets/shop/order.jpg';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../../Hooks/useMenu';
+import FoodCard from '../../../Components/SectionTitle/FoodCard/FoodCard';
 
 const Order = () => {
-    const [tabIndex,setTabIndex] = useState(0)
+    const [tabIndex, setTabIndex] = useState(0)
+    const [menu] = useMenu()
+    //  all items filter 
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+
     return (
         <div>
             <Cover title="Order Food" img={orderCoverImg}></Cover>
@@ -16,9 +26,15 @@ const Order = () => {
                     <Tab>Soup</Tab>
                     <Tab>Dessert</Tab>
                     <Tab>Drinks</Tab>
-                    
+
                 </TabList>
-                <TabPanel></TabPanel>
+                <TabPanel>
+                    <div className='container mx-auto grid md:grid-cols-3 gap-10 my-8'>
+                        {
+                            salad.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
+                        }
+                    </div>
+                </TabPanel>
                 <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
                 <TabPanel></TabPanel>
