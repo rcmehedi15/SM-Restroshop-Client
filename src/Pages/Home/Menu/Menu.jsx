a4e76842e2fced3f586cab54be713c4c9d65e6aa
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-27 12:41:42 +0600

    menu

--- src/Pages/Home/Menu/Menu.jsx
+++ src/Pages/Home/Menu/Menu.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/menu-bg.jpg'
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import useMenu from '../../../Hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from './MenuCategory/MenuCategory';
 
 const Menu = () => {
-    
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+
+ 
     return (
         <div>
             <Helmet>
@@ -12,8 +23,12 @@ const Menu = () => {
                     Menu | SM RESTRO SHOP
                 </title>
             </Helmet>
-            <Cover img={menuImg} title="Our Menu"></Cover>
-           
+            {/* main cover  */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offered menu items  */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* desserts menu items  */}
+            <MenuCategory items={desserts} title="Dessert" img={dessertImg}></MenuCategory>
         </div>
     );
 };
--- src/Pages/Home/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Home/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import MenuItem from '../../../Shared/MenuItem/MenuItem';
+import Cover from '../../../Shared/Cover/Cover';
 
-const MenuCategory = () => {
+const MenuCategory = ({items,title,img}) => {
     return (
         <div>
-            
+            {title && <Cover img={img} title="Our Menu"></Cover>}
+
+             <div className='container mx-auto grid md:grid-cols-2 gap-4'>
+                {
+                    items.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
