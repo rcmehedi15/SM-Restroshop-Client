042acb0f8a40bb50a0e258d9c7fd90e7d9fdd030
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-05-28 12:07:44 +0600

    footer logo change and pagination setup

--- src/Pages/Order/OrderTab.jsx
+++ src/Pages/Order/OrderTab.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import FoodCard from '../../Components/SectionTitle/FoodCard/FoodCard';
-
-const OrderTab = ({items}) => {
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+const OrderTab = ({ items }) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    };
     return (
-        <div className='container mx-auto grid md:grid-cols-3 gap-10 my-8'>
-            {
-                items.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
-            }
+        <div >
+
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            items.map(item => <FoodCard
+                                key={item._id}
+                                item={item}
+                            ></FoodCard>)
+                        }
+                    </div>
+
+                </SwiperSlide>
+
+            </Swiper>
         </div>
     );
 };
