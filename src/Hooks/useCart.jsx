59cdf354d45740591760a55b9100a88e1508fde8
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-06-04 00:36:09 +0600

    jwt admin done

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -1,15 +1,27 @@
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from 'react';
-import { AuthContext } from '../Providers/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
+import useAuth from './UseAuth';
 
 const useCart = (email) => {
-    const { user } = useContext(AuthContext);
-    const { isLoading, refetch, data: cart = [] } = useQuery({
+    const { user,loading } = useAuth();
+    
+    // const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
+    const {refetch, data: cart = [] } = useQuery({
         queryKey: ['cart', user?.email],
+        enabled: !loading,
         queryFn: async () => {
-            const response = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
-            return response.json();
+            const res = await axiosSecure(`/carts?email=${user?.email}`)
+            console.log('res from axios', res);
+            return res.data;
         },
+        // queryFn: async () => {
+        //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`,{
+        //         headers: {
+        //             authorization: `bearer ${token}`
+        //         }})
+        //     return res.json();
+        // },
     })
     return [cart, refetch]
 
--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link, NavLink, Outlet } from 'react-router-dom';
-import { FaShoppingCart, FaWallet, FaCalendar, FaHome,FaUtensils,FaBook,FaUsers   } from 'react-icons/fa';
+import { FaShoppingCart, FaWallet, FaCalendar, FaHome, FaUtensils, FaBook, FaUsers } from 'react-icons/fa';
 import useCart from '../../Hooks/useCart';
+import useAdmin from '../../Hooks/useAdmin';
 
 const Dashboard = () => {
     const [cart] = useCart();
-    const isAdmin = true;
+    // const isAdmin = true;
+    const [isAdmin] = useAdmin();
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
